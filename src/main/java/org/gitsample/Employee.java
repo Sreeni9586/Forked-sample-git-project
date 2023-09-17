bc1ff37f3b4ebe7dea440cd529d6534c15141b1c
Sreeni9586 <143741943+Sreeni9586@users.noreply.github.com>
2023-09-17 14:50:44 +0530

    Update Employee.java
    Updated Address Variables

--- src/main/java/org/gitsample/Employee.java
+++ src/main/java/org/gitsample/Employee.java
@@ -2,9 +2,9 @@ package org.gitsample;
 
 public class Employee {
 	private String name;
-	private String address ;
-	private String address2 ;
-	private String address3 ;
+	private String address-1 ;
+	private String address-2 ;
+	private String address-3 ;
 	private String PinCode ;
 	
 	
